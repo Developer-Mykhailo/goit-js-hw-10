a264ee75893b2dc0c3d617d92e11f45a4394f347
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-05-08 05:01:45 -0600

    task-2 done / add styles

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,47 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const delay = event.target.elements.delay.value;
+  const isActive = event.target.elements.state.value;
+
+  getResult(delay, isActive)
+    .then(() => {
+      iziToast.show({
+        message: `✅  Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        backgroundColor: 'RGBA(51, 198, 130, 0.9)',
+        messageColor: 'white',
+        icon: null,
+        progressBar: false,
+      });
+    })
+    .catch(() => {
+      iziToast.show({
+        message: `❌  Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        backgroundColor: 'RGBA(254, 85, 73, 0.9)',
+        messageColor: 'white',
+        icon: null,
+        progressBar: false,
+      });
+    });
+}
+
+function getResult(delay, isActive) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isActive === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+  return promise;
+}
